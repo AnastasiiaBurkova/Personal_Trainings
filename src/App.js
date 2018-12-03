aa4d7600c1478b0a1b39ef2fe0e29f8055bbdbc1
a1702633 <42831059+a1702633@users.noreply.github.com>
2018-12-04 01:48:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Customers from "./components/Customers";
+import Trainings from "./components/Trainings";
+import Calendar from "./components/Calendar";
+import Home from "./components/Home";
+import { BrowserRouter, Router, Route, Link, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <div>
+            <div className="navbar">
+              <div className="container">
+                <Link to="home" className="redirects">
+                  Home
+                </Link>{" "}
+                <Link to="customers" className="redirects">
+                  Customers
+                </Link>{" "}
+                <Link to="trainings" className="redirects">
+                  Trainings
+                </Link>{" "}
+                <Link to="calendar" className="redirects">
+                  Calendar
+                </Link>{" "}
+              </div>
+            </div>
+            <Switch>
+              <Route path="/customers" component={Customers} />
+              <Route path="/trainings" component={Trainings} />
+              <Route path="/calendar" component={Calendar} />
+              <Route path="/home" component={Home} />
+
+              <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
